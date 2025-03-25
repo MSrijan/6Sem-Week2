78bbaf34957d4dadb3c4372fe855943a4ec51297
Srijan <srijanm20@gmail.com>
2025-03-25 08:05:45 +0545

    dbcontext

---
+++ Week2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Week2.Entities;
+
+namespace Week2.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
